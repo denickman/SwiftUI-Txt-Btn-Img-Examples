8668a2b73e5724a1ad6c176d2816a2a367e467c9
Denis <denis.yaremenko@huma.com>
2024-03-03 13:08:13 +0200

    Initial Commit

---
+++ First/FirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstApp.swift
+//  First
+//
+//  Created by Denis Yaremenko on 03.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct FirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
